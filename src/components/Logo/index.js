04db0d6d2c2d3f0609c7e1f17ca7e4c7bc32aa4f
Lucas Gonçalves <lucasgnclvs1@gmail.com>
2024-07-17 19:14:34 -0300

    creating application and modeling first component: logo

---
+++ src/components/Logo/index.js
@@ -0,0 +1,13 @@
+import './style.css'
+import logo from '../../img/logo.svg';
+
+function Logo() {
+    return (
+        <div className="logo">
+            <img src={logo} className="logo-react" alt="logo"></img>
+            <p><strong>Library</strong>React</p>
+        </div>
+    )
+}
+
+export default Logo;
